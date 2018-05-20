75c2125b895c9bd1056fc275c9f45807068d6979
Ahamed <ahamedali95@gmail.com>
2018-05-20 00:20:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file
+
+
+function findMatching(drivers, string) {
+  return drivers.filter(function(driverName) {
+    return driverName === string || driverName === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driverName) {
+    return driverName[driverName.length - 1] !== string && driverName.indexOf(string) !==  Math.floor(driverName.length / 2) && driverName.indexOf(string) !== -1;
+  });
+}
+
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driverObj) {
+    return driverObj.name === string;
+  });
+}
